2505fe542f19b6e2e74184deb020d148bccafa46
Eliane Orlandin <liorlandin33@gmail.com>
2024-09-09 21:06:59 -0300

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Bot de Telegram voltado para Analise de Dados do Salão Stella.',
+    author='Eliane Orlandin do Carmo',
+    license='',
+)
