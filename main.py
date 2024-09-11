0468afd7a233d3e07de29c30636b24967695c38f
Eliane Orlandin <liorlandin33@gmail.com>
2024-09-11 09:07:08 -0300

    ✨ Trabalhando com o bot do Google Sheets

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
 from src.telegramBot import TelegramBot
+from src.driveBot import driveBot
+from dotenv import load_dotenv
+import os
 
-bot = TelegramBot()
-bot.start()
+#bot = TelegramBot()
+#bot.start()
+load_dotenv()
+
+# Initialize the Drive Bot class
+driveBot = driveBot()
+
+# Get the data from the Google Drive
+drive_data = driveBot.get_data()
+
+# Print the data for demonstration purposes
+print(f"Data retrieved: {drive_data}")
+print(drive_data)
